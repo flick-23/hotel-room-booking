6dcebe5425150bc2e6b14aeedfe8ab3e598bba8e
Venkatesh D <venkigd8@gmail.com>
2022-06-21 23:51:35 +0530

    Frontend of login and register

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Homescreen from "./screens/Homescreen";
 import Bookingscreen from "./screens/Bookingscreen";
+import Registerscreen from "./screens/Registerscreen";
+import Loginscreen from "./screens/Loginscreen";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Homescreen />} />
+          <Route path="/register" element={<Registerscreen />} />
+          <Route path="/login" element={<Loginscreen />} />
           <Route path="/book/:roomid" element={<Bookingscreen />} />
         </Routes>
       </BrowserRouter>
---
+++ client/src/screens/Registerscreen.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+
+function Registerscreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCPassword] = useState("");
+
+  const register = () => {
+    if (password == cpassword) {
+      const user = {
+        name,
+        email,
+        password,
+        cpassword,
+      };
+      console.log(user);
+    } else {
+      alert("Passwords do not match!");
+    }
+  };
+
+  return (
+    <div>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5">
+          <div className="bs">
+            <h2>Register</h2>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confrim Password"
+              value={cpassword}
+              onChange={(e) => setCPassword(e.target.value)}
+            ></input>
+
+            <button className="btn btn-primary  mt-3" onClick={register}>
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Registerscreen;
