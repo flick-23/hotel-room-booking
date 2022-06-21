96ec0fc8d6700122a4e0f6fd86804be2730e0b1b
Venkatesh D <venkigd8@gmail.com>
2022-06-21 22:35:38 +0530

    Book room frontend

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 function Bookingscreen() {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [room, setRoom] = useState();
   const params = useParams();
@@ -26,8 +26,44 @@ function Bookingscreen() {
   }, []);
 
   return (
-    <div>
-      <h1>Room id={params.roomid}</h1>
+    <div className="m-5">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h1>Error</h1>
+      ) : (
+        <div>
+          <div className="row justify-content-center mt-5 bs">
+            <div className="col-md-6">
+              <h1>{room.name}</h1>
+              <img src={room.imageurls[0]} className="bigimg" />
+            </div>
+            <div className="col-md-6">
+              <div style={{ textAlign: "right" }}>
+                <h1>Booking Details</h1> <hr />
+                <b>
+                  <p>Name : Flick 23</p>
+                  <p>From Date : From date</p>
+                  <p>To Date : To date</p>
+                  <p>Max count : {room.maxcount}</p>
+                </b>
+              </div>
+              <div style={{ textAlign: "right" }}>
+                <h1>Amount</h1>
+                <hr />
+                <b>
+                  <p>Total days: </p>
+                  <p>Rent per day: </p>
+                  <p>Total Amount: </p>
+                </b>
+              </div>
+              <div style={{ float: "right" }}>
+                <button className="btn btn-primary">Pay Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
