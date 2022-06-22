6dda9612a124acdf96331b1ac1c034fb6db1108a
Venkatesh D <venkigd8@gmail.com>
2022-06-23 00:46:51 +0530

    Get all rooms and bookings

--- client/src/screens/Adminscreen.js
+++ client/src/screens/Adminscreen.js
@@ -20,7 +20,7 @@ function Adminscreen() {
           <Bookings />
         </TabPane>
         <TabPane tab="Rooms" key="2">
-          Rooms
+          <Rooms />
         </TabPane>
         <TabPane tab="Add Room" key="3">
           Add Room
@@ -59,8 +59,159 @@ export function Bookings() {
 
   return (
     <div className="row">
-      {loading && <Loader />}
-      {bookings.length && <h1>Total bookings: {bookings.length}</h1>}
+      <div>
+        {loading && <Loader />}
+        {bookings.length && (
+          <h1>
+            <center>Bookings</center>
+          </h1>
+        )}
+        <table className="table table-bordered table-dark">
+          <thead className="bs thead-dark">
+            <tr>
+              <th>Booking Id</th>
+              <th>User Id</th>
+              <th>Room</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.length &&
+              bookings.map((booking) => {
+                return (
+                  <tr>
+                    <td>{booking._id}</td>
+                    <td>{booking.userid}</td>
+                    <td>{booking.room}</td>
+                    <td>{booking.fromDate}</td>
+                    <td>{booking.toDate}</td>
+                    <td>{booking.status}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export function Rooms() {
+  const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.get("/api/rooms/getAllRooms");
+        setRooms(data.data.rooms);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="row">
+      <div>
+        {loading && <Loader />}
+        {rooms.length && (
+          <h1>
+            <center>Rooms</center>
+          </h1>
+        )}
+        <table className="table table-bordered table-dark">
+          <thead className="bs thead-dark">
+            <tr>
+              <th>Room Id</th>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Rent Per Day</th>
+              <th>Max Count</th>
+              <th>Ph Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rooms.length &&
+              rooms.map((room) => {
+                return (
+                  <tr>
+                    <td>{room._id}</td>
+                    <td>{room.name}</td>
+                    <td>{room.type}</td>
+                    <td>{room.rentperday}</td>
+                    <td>{room.maxcount}</td>
+                    <td>{room.phonenumber}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export function User() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await (await axios.get("/api/users/getallusers")).data;
+        setUsers(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="row">
+      <div>
+        {loading && <Loader />}
+        {users.length && (
+          <h1>
+            <center>Users</center>
+          </h1>
+        )}
+        <table className="table table-bordered table-dark">
+          <thead className="bs thead-dark">
+            <tr>
+              <th>User Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length &&
+              users.map((user) => {
+                return (
+                  <tr>
+                    <td>{user._id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.isAdmin}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
